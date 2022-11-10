a3a1f2d87c69c12a194042a3239794ad9343606e
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-10 16:13:40 +0100

    Struttura Header > container > box-left > img box-right > a

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -9,6 +9,28 @@ export default {
 
 <template>
     <header id="site-header">
+        <div class="container">
+
+            <div class="box_left">
+                <img src="" alt="">
+            </div>
+            <!-- /.box_left -->
+
+            <div class="box_right">
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+            <!-- /.box_right -->
+        </div>
+        <!-- /.container -->
 
     </header>
 </template>
