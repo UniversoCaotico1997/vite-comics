991703e782edb741f098d233ab3db689b8830b20
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-11-11 17:16:21 +0100

    modifica components RowThumbnails SiteMain

---
+++ src/components/RowThumbnails.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+    name: `RowThumbnails`
+}
+
+</script>
+
+
+
+
+<template>
+    <div class="row">
+        <div class="col">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis facilis natus quae tempore
+            eaque est impedit quaerat tempora aperiam sit praesentium voluptate, suscipit autem eos quidem
+            officia, accusantium ex cum?
+            <h3>Titolo</h3>
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+.row {
+    display: flex;
+    gap: 1rem;
+}
+
+.col {
+    width: calc(100% / 6);
+
+    h3 {
+        margin-top: 0.50rem;
+    }
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,12 +1,14 @@
 <script>
 import SectionBanner from "./SectionBanner.vue";
-import SectionContent from "./SectionContent.vue";
+import SectionContent from "./RowThumbnails.vue";
+import RowThumbnails from "./RowThumbnails.vue";
 
 export default {
     name: `SiteMain`,
     components: {
         SectionBanner,
-        SectionContent
+        SectionContent,
+        RowThumbnails
     }
 
 }
@@ -17,9 +19,11 @@ export default {
 <template>
 
     <main id="site-main">
-
-
-        <SectionContent></SectionContent>
+        <section class="content">
+            <div class="container">
+                <RowThumbnails></RowThumbnails>
+            </div>
+        </section>
 
 
         <SectionBanner></SectionBanner>
@@ -30,5 +34,17 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.content {
+    background-color: #1C1C1C;
 
+    .container {
+        width: 80%;
+        margin: auto;
+        padding: 2rem;
+        display: flex;
+        justify-content: space-between;
+        color: white;
+    }
+
+}
 </style>
